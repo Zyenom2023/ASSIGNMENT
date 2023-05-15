8b2846ef91bb95127a25134699d1ba6b9579f4a8
Zyenom2023 <131315460+Zyenom2023@users.noreply.github.com>
2023-05-15 06:35:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
+#question number one#
 def find_largest_number(numbers):
     if len(numbers) == 0:
         return None  # Return None if the list is empty
-
     largest_number = numbers[0]  # Assume the first number is the largest
-
     for number in numbers:
         if number > largest_number:
             largest_number = number  # Update the largest number if a larger number is found
-
     return largest_number
-
 # Example usage
 numbers = [12, 43, 7, 99, 22, 36]
 largest = find_largest_number(numbers)
 
+#Question number two#
 print("The largest number is:", largest)
 def print_primes(n):
     # A prime number is greater than 1 and only divisible by 1 and itself
@@ -23,7 +21,14 @@ def print_primes(n):
                 break
         else:
             print(num)
-
 # Example usage
 print_primes(20)
 
+#Question number three#
+def reverse_number(number):
+    reversed_number = 0
+
+    while number != 0:
+        digit = number % 10
+        reversed_number = (reversed_number * 10) + digit
+        number //= 10
