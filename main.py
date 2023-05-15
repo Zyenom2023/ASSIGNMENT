a2aebe1a867c48b27212f17a127cd5345e067e1e
Domshak007 <132787417+Domshak007@users.noreply.github.com>
2023-05-15 06:48:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -37,4 +37,17 @@ number = 12345
 reversed = reverse_number(number)
 print("Reversed number:", reversed)
 
-#Question number four
+#Question number four#
+def calculate_sum(numbers):
+    sum = 0
+    for number in numbers:
+        sum += number
+    return sum
+# Example usage
+numbers = [1, 2, 3, 4, 5]
+total_sum = calculate_sum(numbers)
+print("The sum of numbers is:", total_sum)
+
+#Question number five#
+
+
